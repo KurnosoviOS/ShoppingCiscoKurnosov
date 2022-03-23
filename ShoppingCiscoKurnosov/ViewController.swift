16ca8c005b0a06f0d11f2ae16639f2be4e6e3749
Kurnosov <akthesnipe@gmail.com>
2022-03-23 18:09:43 +0500

    Initial Commit

---
+++ ShoppingCiscoKurnosov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingCiscoKurnosov
+//
+//  Created by Алексей Курносов on 23.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ShoppingCiscoKurnosovUITests/ShoppingCiscoKurnosovUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ShoppingCiscoKurnosovUITestsLaunchTests.swift
+//  ShoppingCiscoKurnosovUITests
+//
+//  Created by Алексей Курносов on 23.03.2022.
+//
+
+import XCTest
+
+class ShoppingCiscoKurnosovUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
